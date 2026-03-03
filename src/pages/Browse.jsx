e398ff7737bfb6d2e45901128c8dae7fe57a1a50
Hadookhan <Hadikhan05@hotmail.co.uk>
2026-03-03 12:26:47 +0000

    Initial pages

---
+++ src/pages/Browse.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function Browse() {
+
+    return (
+        <div>
+            <p>hello from browse</p>
+        </div>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+
+  return (
+      <div>
+          <p>hello from home</p>
+      </div>
+  );
+}
