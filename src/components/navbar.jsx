f5ee31d1d288387ed813dbfe644e994205e5346d
NazimMiah-code <Nazimmiah77@gmail.com>
2026-04-16 02:39:35 +0100

    navbar
    set up routing and navbar and the css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,20 @@
-import {    Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 function Navbar() {
-    return (
-        <header className="navbar">
+  return (
+    <header className="navbar">
       <Link to="/" className="logo">
         Movies4US <span className="logo-dot">•</span>
       </Link>
 
       <nav className="nav-links">
-        <Link to="/" className="nav-link active">
-          Home
-        </Link>
-
-        <a href="#" className="nav-link">
-          Browse
-        </a>
-
-        <a href="#" className="nav-link">
-          Search
-        </a>
-
-        <a href="#" className="nav-link">
-          Actors
-        </a>
-
-        <a href="#" className="nav-link">
-          Directors
-        </a>
-
-        <a href="#" className="nav-link">
-          Watchlist
-        </a>
+        <Link to="/" className="nav-link active">Home</Link>
+        <Link to="/browse" className="nav-link">Browse</Link>
+        <Link to="/search" className="nav-link">Search</Link>
+        <Link to="/info" className="nav-link">Actors</Link>
+        <Link to="/info" className="nav-link">Directors</Link>
+        <a href="#" className="nav-link">Watchlist</a>
       </nav>
 
       <div className="profile-circle">◌</div>
